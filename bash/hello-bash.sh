4b75e48ee4ca6caebb9d0c1fcc2f0a2c6c47a92b
Doctype Human <doctypehuman@gmail.com>
2020-10-03 14:16:27 +0200

    Hello Bash

---
+++ bash/hello-bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# In order to be effecient in daily sys admin tasks, bash scripting is required.
+# While I am preparing for my RHCSA, it will be beneficial for me to learn bash scripting as well.
+
+# Hello Bash 
+
+echo "Hello Bash"
+
+# This file has been saved as bash.sh It does not have to be done like this but I am working on it step by step. the next file will have some more changes.
+
+
+
